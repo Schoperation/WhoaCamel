2584c80049b140dfbc3c5ddd1f41bbce980a89c9
Schoperation <cschop1@gmail.com>
2022-07-13 21:19:52 -0400

    Added clearScreen

---
+++ art/loadart.go
@@ -0,0 +1,9 @@
+package art
+
+/*
+	Loads in all of our art from the files. I put them in text files so they're easier to edit.
+*/
+
+func loadTexts() {
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("WHOOOOAA CAMEL WHOOOOOOOOOA!!")
+
+	// Load all of our stuff
+	fmt.Println("Loading stuff...")
+
+	clearScreen()
+
+	// Say our "welcome"
+	fmt.Println("Whoa Camel!")
+	fmt.Println("...is what you'll be saying as you cruise through this game!")
+	fmt.Println("Well, 'cruise' might be too strong of a word... oh well.")
+}
+
+func clearScreen() {
+
+	cmd := exec.Command("clear")
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	}
+
+	// Ensure that the output of the cmd goes to Go's output
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
